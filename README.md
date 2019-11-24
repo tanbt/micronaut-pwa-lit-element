65c6ee1621e7f46c52b551b9175d58c86f0d72ad
Tan Bui <tan@vaadin.com>
2019-11-24 17:09:42 +0200

    Create server API

---
+++ README.md
@@ -0,0 +1,8 @@
+# Micronaut PWA with Lit-element
+A simple project with `micronaut` and `lit-element`.
+
+
+## Run
+* `./gradlew server:run`
+
+    Test: http://localhost:8080/Tan
---
+++ server/src/main/java/io/micronaut/HelloController.java
@@ -0,0 +1,14 @@
+package io.micronaut;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/")
+public class HelloController {
+
+    @Get("/{name}")
+    HttpResponse<String> hello(String name) {
+        return HttpResponse.ok("Hello, " + name + "!");
+    }
+}
